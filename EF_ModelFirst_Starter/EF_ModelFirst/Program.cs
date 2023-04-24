7ca38471a2dc99a661e2f7abd8ec02db6b5903fc
Andrew Ma <ma.andrew2984@gmail.com>
2023-04-24 16:24:51 +0100

    Fix

--- EF_ModelFirst_Starter/EF_ModelFirst/Program.cs
+++ EF_ModelFirst_Starter/EF_ModelFirst/Program.cs
@@ -2,16 +2,15 @@
 using System;
 using System.Linq;
 
-namespace EF_ModelFirst
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            using (var db = new SouthwindContext())
-            {
+namespace SouthWindProject;
 
-            }
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var db = new SouthwindContext())
+        {
+
         }
     }
 }
