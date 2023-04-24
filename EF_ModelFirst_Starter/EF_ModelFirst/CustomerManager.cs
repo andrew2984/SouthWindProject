01446c87fdb44ec46339497663ddfdbb5e7c0557
JSB-22 <j.s.banyard@gmail.com>
2023-04-24 17:59:16 +0100

    Added delete entry

--- EF_ModelFirst_Starter/EF_ModelFirst/CustomerManager.cs
+++ EF_ModelFirst_Starter/EF_ModelFirst/CustomerManager.cs
@@ -9,5 +9,15 @@ namespace SouthWindProject
     public static class CustomerManager
     {
         private static SouthwindContext db = new SouthwindContext();
+
+        public static void DeleteEntry(string customerId)
+        {
+            using (db)
+            {
+                var customer = db.Customers.Find(customerId);
+                db.Customers.Remove(customer);
+                db.SaveChanges();
+            }
+        }
     }
 }
