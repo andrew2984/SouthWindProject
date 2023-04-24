65894c50e2acaff0537dbcd8b62e33e95906e7fe
JSB-22 <j.s.banyard@gmail.com>
2023-04-24 17:31:19 +0100

    Added seed

---
+++ EF_ModelFirst_Starter/EF_ModelFirst/CustomerManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SouthWindProject
+{
+    public static class CustomerManager
+    {
+        private static SouthwindContext db = new SouthwindContext();
+    }
+}
